6ad795b49f5eadfe9dba167a5229f475939c3e7b
DESKTOP-OEIK6IO\lenul <lenule.vasickova@gmail.com>
2022-02-20 20:50:08 +0100

    úloha2

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 // console.log('test');
 
 let nadpis = document.querySelector('h1');
+let odstavec = document.querySelector('p');
+let audioFile = document.getElementById('zvukovaStopa')
+
 nadpis.classList.add('zluty');
 nadpis.classList.remove('zluty')
 
@@ -26,7 +29,43 @@ function stiskKlavesy(udalost) {
 
 
 function startAudio() {
-    let audioFile = document.getElementById('zvukovaStopa')
+
     console.log('Zapínám píseň')
     audioFile.play();
+}
+
+function pause() {
+    console.log('Pauzni')
+    audioFile.pause();
+}
+
+function load() {
+    console.log('Reset')
+    audioFile.load();
+}
+
+function Zvuk0 () {
+    console.log('Zvuk0')
+    audioFile.volume = 0.0;
+}
+
+function Zvuk05 () {
+    console.log('Zvuk0,5')
+    audioFile.volume = 0.5;
+}
+
+function Zvuk1 () {
+    console.log('Zvuk1,0')
+    audioFile.volume = 1.0;
+}
+
+function priKliknuti() {
+    let nadpis = document.querySelector('p');
+    nadpis.style.color = 'red';
+}
+
+let zaklad = 16 
+function vetsiPriKliknuti() {
+    zaklad++;
+    odstavec.style.fontSize = zaklad + 'px'
 }
